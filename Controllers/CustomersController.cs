a69eaee6052a635f0bdacf17267239f98d8dd9a3
Daniel Cassin <dcassin2082@gmail.com>
2022-06-09 14:57:37 -0700

    Add files via upload

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebApplication16.Models;
+using WebApplication16.Services;
+
+namespace WebApplication16.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerService _customerService;
+
+        public CustomersController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        // GET: api/Customers
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Customer>>> GetCustomers()
+        {
+            return await _customerService.GetCustomers();
+        } 
+
+        // GET: api/Customers/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Customer>> GetCustomer(int id)
+        {
+            var customer = await _customerService.GetCustomer(id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return customer;
+        }
+
+        // PUT: api/Customers/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCustomer(int id, Customer customer)
+        {
+            if (id != customer.CustomerId)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+                await _customerService.PutCustomer(customer);
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!(await CustomerExists(id)))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Customers
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Customer>> PostCustomer(Customer customer)
+        {
+            await _customerService.PostCustomer(customer);
+
+            return CreatedAtAction("GetCustomer", new { id = customer.CustomerId }, customer);
+        }
+
+        // DELETE: api/Customers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer(int id)
+        {
+            var customer = await _customerService.GetCustomer(c => c.CustomerId.Equals(id));
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            await _customerService.DeleteCustomer(customer);
+
+            return NoContent();
+        }
+
+        [HttpGet("exists/id")]
+        public async  Task<bool> CustomerExists(int id)
+        {
+            var customers = await _customerService.GetCustomers(c => c.CustomerId.Equals(id));
+
+            return customers.Count > 0;
+        }
+    }
+}
